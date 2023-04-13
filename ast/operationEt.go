3bcec35af529c6a2e7fcebc41c5d3d8b41349667
thibault-cne <thibault.chene23@gmail.com>
2023-04-13 16:12:38 +0200

    feat: operation et

--- ast/operationEt.go
+++ ast/operationEt.go
@@ -2,42 +2,42 @@ package ast
 
 import "ogtiger/parser"
 
-type OperationOu struct {
+type OperationEt struct {
 	Left Ast
 	Right []Ast 
 }
 
-func (e OperationOu) Display() string {
+func (e OperationEt) Display() string {
 	return " operationOu"
 }
 
-func  (l *AstCreatorListener) OperationOuEnter(ctx parser.IOperationOuContext) {
+func  (l *AstCreatorListener) OperationEtEnter(ctx parser.IOperationEtContext) {
 	// l.AstStack = append(l.AstStack, &ExprOu{})
 }
 
-func  (l *AstCreatorListener) OperationOuExit(ctx parser.IOperationOuContext) {
-	operationOu := &OperationOu{}
+func  (l *AstCreatorListener) OperationEtExit(ctx parser.IOperationEtContext) {
+	operationEt := &OperationEt{}
 
 	if ctx.GetChildCount() == 1 {
 		return
 	}
 
 	// Get the first exprEt
-	exprEt := l.AstStack[len(l.AstStack)-1]
+	operationCompar := l.AstStack[len(l.AstStack)-1]
 
 	// Pop the last element of the stack
 	l.AstStack = l.AstStack[:len(l.AstStack)-1]
 
-	operationOu.Left = exprEt
+	operationEt.Left = operationCompar
 
 	// Get the other exprEt
 	for i := 0; i < (ctx.GetChildCount() - 1) / 2; i++ {
-		operationOu.Right = append(operationOu.Right, l.AstStack[len(l.AstStack)-1])
+		operationEt.Right = append(operationEt.Right, l.AstStack[len(l.AstStack)-1])
 
 		// Pop the last element of the stack
 		l.AstStack = l.AstStack[:len(l.AstStack)-1]
 	}
 
 	// Push the new element on the stack
-	l.AstStack = append(l.AstStack, operationOu)
+	l.AstStack = append(l.AstStack, operationEt)
 }
