727ebf97c4a54f0da74e39f814967107bc245578
thibault-cne <thibault.chene23@gmail.com>
2023-04-19 18:31:12 +0200

    feat: et csem

--- ast/operation_et.go
+++ ast/operation_et.go
@@ -19,8 +19,17 @@ type OperationEt struct {
 }
 
 func (e *OperationEt) VisitSemControl(slt *slt.SymbolTable, L *logger.StepLogger) antlr.ParserRuleContext {
-	e.Left.VisitSemControl(slt, L)
-	e.Right.VisitSemControl(slt, L)
+	leftCtx := e.Left.VisitSemControl(slt, L)
+	rightCtx := e.Right.VisitSemControl(slt, L)
+
+	if !e.Left.ReturnType().Equals(ttype.NewTigerType(ttype.Int)) {
+		L.NewSemanticError("Left side of the et is not an integer", leftCtx)
+	}
+
+	if !e.Right.ReturnType().Equals(ttype.NewTigerType(ttype.Int)) {
+		L.NewSemanticError("Right side of the et is not an integer", rightCtx)
+	}
+
 	return e.Ctx
 }
 
